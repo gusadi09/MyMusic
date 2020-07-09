c126a8a03c66d9d3adca00ac5fa4a268ae22fd51
gusadi <gusadi872@gmail.com>
2020-07-10 01:35:19 +0800

    Final Tested

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    private var _player: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        guard let url = Bundle.main.url(forResource: "guitar", withExtension: "wav") else {return}
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback,mode: .default)
+            _player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 
 
+    @IBAction func playPressed(_ sender: UIButton) {
+        guard let player = _player else {return}
+        player.play()
+        stopButton.enable()
+        playButton.disable()
+    }
+    @IBAction func stopPressed(_ sender: UIButton) {
+        guard let player = _player else {return}
+        player.stop()
+        playButton.enable()
+        stopButton.disable()
+    }
 }
 
+extension UIButton {
+    func enable() {
+        self.isEnabled = true
+    }
+    
+    func disable() {
+        self.isEnabled = false
+    }
+}
